a88290ceffb184b76cfccefcf0ff86c31020d167
CrossField1973 <45197889+CrossField1973@users.noreply.github.com>
2020-10-24 16:21:00 +0200

    Changed from DHBW Coin to PadlockToken

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,11 @@
 const Migrations = artifacts.require("Migrations");
-const DHBWCoin = artifacts.require("DHBWCoin");
-const DHBWVoting = artifacts.require("DHBWVoting");
+// const DHBWCoin = artifacts.require("DHBWCoin");
+// const DHBWVoting = artifacts.require("DHBWVoting");
+const PadlockToken = artifacts.require("DHBWVoting");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(DHBWCoin);
-  deployer.deploy(DHBWVoting);
+  // deployer.deploy(DHBWCoin);
+  // deployer.deploy(DHBWVoting);
+  deployer.deploy(PadlockToken);
 };
