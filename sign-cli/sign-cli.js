94f923c5b8cefdb0e60145ce2a1d1ada1bea198c
lkrauter <lkrauter@stone-e.de>
2020-11-28 17:20:42 +0100

    adjust sign cli, propmpts added

--- sign-cli/sign-cli.js
+++ sign-cli/sign-cli.js
@@ -1,30 +1,54 @@
-console.log('hallo ich bin ein Schloss !');
 const openpgp = require('openpgp');
+const readline = require("readline");
+const fs = require("fs");
+const { exit } = require('process');
 
-const keyStore =require("./pgp_testVals");
+const read_line = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let privateKeyRaw = ""
 
-(async () => {
+console.log('Willkommen beim automatisch Nachricht signier Programm');
 
-    const { keys: [privateKey] } = await openpgp.key.readArmored(keyStore.privateKeyArmored);
-    console.log(privateKey)
-    await privateKey.decrypt(keyStore.passphrase);
-
-    const { data: cleartext } = await openpgp.sign({
-        message: openpgp.cleartext.fromText('2020-11-23 13:44'), // CleartextMessage or Message object
-        privateKeys: [privateKey]                             // for signing
-    });
-    console.log(cleartext); // '-----BEGIN PGP SIGNED MESSAGE ... END PGP SIGNATURE-----'
-
-
-
-    const verified = await openpgp.verify({
-        message: await openpgp.cleartext.readArmored(cleartext),           // parse armored message
-        publicKeys: (await openpgp.key.readArmored(keyStore.publicKeyArmored)).keys // for verification
-    });
-    const { valid } = verified.signatures[0];
-    if (valid) {
-        console.log('signed by key id ' + verified.signatures[0].keyid.toHex());
-    } else {
-        throw new Error('signature could not be verified');
+read_line.question("please insert the complete path to your opgp private key: \n", function (path) {
+    if (!fs.lstatSync(path).isFile()) {
+        console.log("Wrong Path. Restart Programm")
+        exit(1)
     }
-})();
+
+    fs.readFile(path, function (err, data) {
+        if (err) {
+            console.log(err);
+            exit(1)
+        }
+        privateKeyRaw = data;
+
+
+        read_line.question("Please insert your private passphrase: \t", function (passphrase) {
+            encrypt_message(passphrase);
+
+        })
+    })
+})
+
+function encrypt_message(passphrase) {
+
+    (async () => {
+        const timeStamp=Date.now().toString()
+        console.log("encrypting Timestamp: ",timeStamp)
+
+        const { keys: [privateKey] } = await openpgp.key.readArmored(privateKeyRaw);
+        await privateKey.decrypt(passphrase);
+
+        const { data: cleartext } = await openpgp.sign({
+            message: openpgp.cleartext.fromText(timeStamp), // CleartextMessage or Message object
+            privateKeys: [privateKey]                             // for signing
+        });
+        console.log("The following is your signed message. Copy it in the singed message field")
+        console.log(cleartext); // '-----BEGIN PGP SIGNED MESSAGE ... END PGP SIGNATURE-----'
+
+    })();
+
+    exit(0);
+}
